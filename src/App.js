5e2b05f0122887470c6c5f27ff76198909362b58
gomez09.mx@gmail.com <gomez09.mx@gmail.com>
2018-08-17 21:05:01 -0500

    agregue jumbotron

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "jquery/dist/jquery.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
 
-import Navbar from './components/Navbar/Navbar';
+import Navbar from "./components/Navbar/Navbar";
+import Jumbotron from "./components/Jumbotron/Jumbotron";
 
 class App extends Component {
   render() {
     return (
       <div>
-        
-         <Navbar/>
-         <main className="container">
+        <Navbar />
+        <main className="container-fluid">
+          <Jumbotron />
         </main>
       </div>
-       
     );
   }
 }
