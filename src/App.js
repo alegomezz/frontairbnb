dff3bcc40852929e76290353d9a535490469ec11
gomez09.mx@gmail.com <gomez09.mx@gmail.com>
2018-08-20 20:30:16 -0500

    agregue routes y home

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "jquery/dist/jquery.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
+import Routes from './Routes';
 
-import Navbar from "./components/Navbar/Navbar";
-import Jumbotron from "./components/Jumbotron/Jumbotron";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <main className="container-fluid">
-          <Jumbotron />
-        </main>
+        <Routes/>
       </div>
     );
   }
